f2ccf605cb876fe138d0a0d92cb600a33fe2c995
Skitbet <54096334+Skitbet@users.noreply.github.com>
2023-09-28 02:15:18 -0400

    created clean.py

---
+++ clean.py
@@ -0,0 +1,15 @@
+import os
+
+def delete_files(directory_path):
+    try:
+        files = os.listdir(directory_path)
+        for file in files:
+            file_path = os.path.join(directory_path, file)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+        print("All files deleted...")
+    except OSError:
+        print("Error occurred...")
+
+directory_path = "C:/Users/Skitbet/Music/"
+delete_files(directory_path)
